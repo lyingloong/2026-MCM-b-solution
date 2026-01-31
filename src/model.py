0a2cc1c3a769426bd872818e66caf182ed97a43d
ANT181603 <wenchaoyu@mail.ustc.edu.cn>
2026-01-31 13:57:50 +0800

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 # 常量定义
 TOTAL_MATERIAL = 100_000_000  # 总材料需求（吨）
@@ -11,10 +12,21 @@ ELEVATOR_ANNUAL_CAPACITY = 179_000  # 每个银河港年运输能力（吨）
 ELEVATOR_COST_PER_TON = 1000  # 每吨运输成本（美元）
 ELEVATOR_RELIABILITY = 0.99  # 可靠性（无故障概率）
 
-#摆渡火箭参数
+# 摆渡火箭参数
 TUG_DELTA_V = 2800 # m/s
 TUG_I_SP = 380 #发动机比冲（秒）
 TUG_RELIABILITY = 0.99 # 可靠性（无故障概率）
+TUG_COST_FUEL_PER = 2 # 燃料价格（美元/千克）
+TUG_COST_VEHICLE = 200_000_000 # 在轨火箭成本
+TUG_N = 50 # 复用次数
+
+# 太空电梯-摆渡火箭单位有效载荷成本
+G_0 = 9.81 # 重力加速度
+R = math.exp(TUG_DELTA_V / (G_0 * TUG_I_SP))
+M_D = 0.2 # 火箭自重和有效载荷比值
+M_PROP = (R-1) * (1+M_D) # 燃料质量
+M_0 = M_PROP + M_D + 1 # 总质量
+COST_ELEVATOR_PER = (ELEVATOR_COST_PER_TON * M_0) / ELEVATOR_RELIABILITY + (TUG_COST_FUEL_PER * M_PROP + TUG_COST_VEHICLE / TUG_N) / TUG_RELIABILITY
 
 # 火箭系统参数
 ROCKET_LAUNCH_SITES = 10  # 火箭发射场数量
@@ -31,7 +43,7 @@ def calculate_scenario_1():
     """Scenario 1: Space Elevator Only"""
     total_annual_capacity = GALACTIC_HARBORS * ELEVATOR_ANNUAL_CAPACITY
     years_needed = np.ceil(TOTAL_MATERIAL / total_annual_capacity)
-    total_cost = TOTAL_MATERIAL * ELEVATOR_COST_PER_TON
+    total_cost = TOTAL_MATERIAL * COST_ELEVATOR_PER
     completion_year = START_YEAR + years_needed
     
     return {
@@ -231,7 +243,7 @@ def save_results_to_file():
                 rocket_annual_capacity = ROCKET_LAUNCH_SITES * ROCKET_LAUNCHES_PER_YEAR_PER_SITE * ROCKET_PAYLOAD_AVG
                 rocket_years = -(-rocket_material // rocket_annual_capacity)  # 向上取整
                 rocket_launches = -(-rocket_material // ROCKET_PAYLOAD_AVG)  # 向上取整
-                rocket_cost = rocket_launches * ROCKET_COST_PER_LAUNCH
+                rocket_cost = rocket_launches * ROCKET_THETA * ROCKET_COST_PER_LAUNCH
             else:
                 rocket_years = 0
                 rocket_cost = 0
